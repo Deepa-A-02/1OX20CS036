62ee09a7f2fec1f005bcec9e6608d681d446e94b
Deepa-A-02 <141942691+Deepa-A-02@users.noreply.github.com>
2023-08-22 14:08:05 +0530

    Add files via upload

---
+++ public/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'></div>
+        <div className='rightSide'></div>
+      <img src={}/>
+          </div>
+  )
+}
+
+export default Navbar
